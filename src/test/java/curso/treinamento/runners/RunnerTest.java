7dda6cd1bd4373e9aaffa6384bf3b3cc9a25367c
Rafael <rafael_occ1@hotmail.com>
2019-08-29 09:30:42 -0300

    Template treinamento

---
+++ src/test/java/curso/treinamento/runners/RunnerTest.java
@@ -0,0 +1,16 @@
+package curso.treinamento.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= {"src/test/resources/features"},
+		glue= {"curso.treinamento.steps"},
+		snippets = SnippetType.CAMELCASE,
+		tags = {})
+
+public class RunnerTest {}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,9 @@
+# language: pt
+
+Funcionalidade: Login
+ 
+  Cenario: Realizar Login com sucesso
+		Dado que eu deseje logar no sistema
+		Quando faco o ligin no sistema "admin@phptravels.com" com a "admin" 
+		Entao sou autenticado
+	
